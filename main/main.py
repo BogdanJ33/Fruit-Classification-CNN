06e064cfc02affed91dfaba0c6b5a269e7168e0d
Bogdan <bogdanjanosevic@gmail.com>
2023-06-23 12:27:52 +0200

    Refactor the code

--- main/main.py
+++ main/main.py
@@ -10,6 +10,7 @@ import keras
 from keras.utils import np_utils
 from keras.models import load_model
 
+IMAGE_SIZE = 40
 def make_image_into_array(path_to_image, data, labels, i):
     images = os.listdir(path_to_image)
     for img in images:
@@ -21,7 +22,7 @@ def make_image_into_array(path_to_image, data, labels, i):
                 imag = cv2.imread(img_path)
                 if imag is not None:
                     img_from_ar = Image.fromarray(imag, 'RGB')
-                    resized_image = img_from_ar.resize((32, 32))
+                    resized_image = img_from_ar.resize((IMAGE_SIZE, IMAGE_SIZE))
                     data.append(np.array(resized_image))
                     labels.append(i)
 
@@ -30,7 +31,7 @@ def make_image_into_array(path_to_image, data, labels, i):
 def convert_to_array(img):
     im = cv2.imread(img)
     img = Image.fromarray(im, 'RGB')
-    image = img.resize((32, 32))
+    image = img.resize((IMAGE_SIZE, IMAGE_SIZE))
     return np.array(image)
 def get_fruit_name(label):
     if label==0:
@@ -62,7 +63,6 @@ def predict_fruit(file, model):
     a.append(ar)
     a=np.array(a)
     score=model.predict(a,verbose=1)
-    print(score)
     label_index=np.argmax(score)
     print(label_index)
     acc=np.max(score)
@@ -70,61 +70,94 @@ def predict_fruit(file, model):
     print(fruit)
     print("The predicted Fruit from file" + file + " is a "+fruit+" with accuracy =    "+str(acc))
 
+def create_model(fruits, labels, num_classes):
+    model = Sequential()
+    model.add(Conv2D(filters=16, kernel_size=2, padding="same", activation="relu", input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3)))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Conv2D(filters=32, kernel_size=2, padding="same", activation="relu"))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Conv2D(filters=64, kernel_size=2, padding="same", activation="relu"))
+    model.add(MaxPooling2D(pool_size=2))
+    model.add(Dropout(0.2))
+    model.add(Flatten())
+    model.add(Dense(512, activation="relu"))
+    model.add(Dropout(0.2))
+    model.add(Dense(num_classes, activation="softmax"))
+    model.summary()
+    model.compile(loss='categorical_crossentropy', optimizer='adam',
+                  metrics=['accuracy'])
+
+    model.fit(fruits, labels, batch_size=32, epochs=40, verbose=1)
+    model.save("model.h5")
+
+def create_train_data_labels():
+    data = []
+    labels = []
+
+    appleFolderPath = "..//MY_data//train//Apple"
+    avocadoFolderPath = "..//MY_data//train//avocado"
+    bananaFolderPath = "..//MY_data//train//Banana"
+    cherryFolderPath = "..//MY_data//train//cherry"
+    kiwiFolderPath = "..//MY_data//train//kiwi"
+    mangoFolderPath = "..//MY_data//train//mango"
+    orangeFolderPath = "..//MY_data//train//orange"
+    pinenappleFolderPath = "..//MY_data//train//pinenapple"
+    strawberriesFolderPath = "..//MY_data//train//strawberries"
+    watermelonFolderPath = "..//MY_data//train//watermelon"
+
+
+
+    data, labels = make_image_into_array(appleFolderPath, data, labels, 0)
+    data, labels = make_image_into_array(avocadoFolderPath, data, labels, 1)
+    data, labels = make_image_into_array(bananaFolderPath, data, labels, 2)
+    data, labels = make_image_into_array(cherryFolderPath, data, labels, 3)
+    data, labels = make_image_into_array(kiwiFolderPath, data, labels, 4)
+    data, labels = make_image_into_array(mangoFolderPath, data, labels, 5)
+    data, labels = make_image_into_array(orangeFolderPath, data, labels, 6)
+    data, labels = make_image_into_array(pinenappleFolderPath, data, labels, 7)
+    data, labels = make_image_into_array(strawberriesFolderPath, data, labels, 8)
+    data, labels = make_image_into_array(watermelonFolderPath, data, labels, 9)
+    fruits = np.array(data)
+    labels = np.array(labels)
+    np.save("fruits", fruits)
+    np.save("labels", labels)
+
+
+def create_test_data_labels():
+    dataTest = []
+    labelsTest = []
+    appleFolderTestPath = "..//MY_data//test//apple"
+    avocadoFolderTestPath = "..//MY_data//test//avocado"
+    bananaFolderTestPath = "..//MY_data//test//banana"
+    cherryFolderTestPath = "..//MY_data//test//cherry"
+    kiwiFolderTestPath = "..//MY_data//test//kiwi"
+    mangoFolderTestPath = "..//MY_data//test//mango"
+    orangeFolderTestPath = "..//MY_data//test//orange"
+    pinenappleFolderTestPath = "..//MY_data//test//pinenapple"
+    strawberriesFolderTestPath = "..//MY_data//test//stawberries"
+    watermelonFolderTestPath = "..//MY_data//test//watermelon"
+
+    dataTest, labelsTest = make_image_into_array(appleFolderTestPath, dataTest, labelsTest, 0)
+    dataTest, labelsTest = make_image_into_array(avocadoFolderTestPath, dataTest, labelsTest, 1)
+    dataTest, labelsTest = make_image_into_array(bananaFolderTestPath, dataTest, labelsTest, 2)
+    dataTest, labelsTest = make_image_into_array(cherryFolderTestPath, dataTest, labelsTest, 3)
+    dataTest, labelsTest = make_image_into_array(kiwiFolderTestPath, dataTest, labelsTest, 4)
+    dataTest, labelsTest = make_image_into_array(mangoFolderTestPath, dataTest, labelsTest, 5)
+    dataTest, labelsTest = make_image_into_array(orangeFolderTestPath, dataTest, labelsTest, 6)
+    dataTest, labelsTest = make_image_into_array(pinenappleFolderTestPath, dataTest, labelsTest, 7)
+    dataTest, labelsTest = make_image_into_array(strawberriesFolderTestPath, dataTest, labelsTest, 8)
+    dataTest, labelsTest = make_image_into_array(watermelonFolderTestPath, dataTest, labelsTest, 9)
+
+    fruitsTest = np.array(dataTest)
+    labelsTest = np.array(labelsTest)
+    np.save("fruitsTest", fruitsTest)
+    np.save("labelsTest", labelsTest)
+
+    return dataTest, labelsTest
 if __name__ == '__main__':
-    # data = []
-    # dataTest = []
-    # labels = []
-    # labelsTest = []
-    # appleFolderPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//train//Apple"
-    # avocadoFolderPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//train//avocado"
-    # bananaFolderPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//train//Banana"
-    # cherryFolderPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//train//cherry"
-    # kiwiFolderPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//train//kiwi"
-    # mangoFolderPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//train//mango"
-    # orangeFolderPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//train//orange"
-    # pinenappleFolderPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//train//pinenapple"
-    # strawberriesFolderPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//train//strawberries"
-    # watermelonFolderPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//train//watermelon"
-    #
-    # appleFolderTestPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//test//apple"
-    # avocadoFolderTestPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//test//avocado"
-    # bananaFolderTestPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//test//banana"
-    # cherryFolderTestPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//test//cherry"
-    # kiwiFolderTestPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//test//kiwi"
-    # mangoFolderTestPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//test//mango"
-    # orangeFolderTestPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//test//orange"
-    # pinenappleFolderTestPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//test//pinenapple"
-    # strawberriesFolderTestPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//test//stawberries"
-    # watermelonFolderTestPath = "C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//test//watermelon"
-    #
-    # data, labels = make_image_into_array(appleFolderPath, data, labels, 0)
-    # data, labels = make_image_into_array(avocadoFolderPath, data, labels, 1)
-    # data, labels = make_image_into_array(bananaFolderPath, data, labels, 2)
-    # data, labels = make_image_into_array(cherryFolderPath, data, labels, 3)
-    # data, labels = make_image_into_array(kiwiFolderPath, data, labels, 4)
-    # data, labels = make_image_into_array(mangoFolderPath, data, labels, 5)
-    # data, labels = make_image_into_array(orangeFolderPath, data, labels, 6)
-    # data, labels = make_image_into_array(pinenappleFolderPath, data, labels, 7)
-    # data, labels = make_image_into_array(strawberriesFolderPath, data, labels, 8)
-    # data, labels = make_image_into_array(watermelonFolderPath, data, labels, 9)
-    # dataTest, labelsTest = make_image_into_array(appleFolderTestPath, dataTest, labelsTest, 0)
-    # dataTest, labelsTest = make_image_into_array(avocadoFolderTestPath, dataTest, labelsTest, 1)
-    # dataTest, labelsTest = make_image_into_array(bananaFolderTestPath, dataTest, labelsTest, 2)
-    # dataTest, labelsTest = make_image_into_array(cherryFolderTestPath, dataTest, labelsTest, 3)
-    # dataTest, labelsTest = make_image_into_array(kiwiFolderTestPath, dataTest, labelsTest, 4)
-    # dataTest, labelsTest = make_image_into_array(mangoFolderTestPath, dataTest, labelsTest, 5)
-    # dataTest, labelsTest = make_image_into_array(orangeFolderTestPath, dataTest, labelsTest, 6)
-    # dataTest, labelsTest = make_image_into_array(pinenappleFolderTestPath, dataTest, labelsTest, 7)
-    # dataTest, labelsTest = make_image_into_array(strawberriesFolderTestPath, dataTest, labelsTest, 8)
-    # dataTest, labelsTest = make_image_into_array(watermelonFolderTestPath, dataTest, labelsTest, 9)
-    # fruits = np.array(data)
-    # labels = np.array(labels)
-    # np.save("fruits", fruits)
-    # np.save("labels", labels)
-    # fruitsTest = np.array(dataTest)
-    # labelsTest = np.array(labelsTest)
-    # np.save("fruitsTest", fruitsTest)
-    # np.save("labelsTest", labelsTest)
+
+    create_train_data_labels()
+    create_test_data_labels()
     fruits = np.load("fruits.npy")
     labels = np.load("labels.npy")
     fruitsTest = np.load("fruitsTest.npy")
@@ -138,34 +171,11 @@ if __name__ == '__main__':
 
     labels = keras.utils.to_categorical(labels, num_classes)
     labelsTest = keras.utils.to_categorical(labelsTest, num_classes_Test)
-    # model = Sequential()
-    # model.add(Conv2D(filters=16, kernel_size=2, padding="same", activation="relu", input_shape=(32, 32, 3)))
-    # model.add(MaxPooling2D(pool_size=2))
-    # model.add(Conv2D(filters=32, kernel_size=2, padding="same", activation="relu"))
-    # model.add(MaxPooling2D(pool_size=2))
-    # model.add(Conv2D(filters=64, kernel_size=2, padding="same", activation="relu"))
-    # model.add(MaxPooling2D(pool_size=2))
-    # model.add(Dropout(0.2))
-    # model.add(Flatten())
-    # model.add(Dense(512, activation="relu"))
-    # model.add(Dropout(0.2))
-    # model.add(Dense(num_classes, activation="softmax"))
-    # model.summary()
-    # model.compile(loss='categorical_crossentropy', optimizer='adam',
-    #               metrics=['accuracy'])
-    #
-    # model.fit(fruits, labels, batch_size=32, epochs=40, verbose=1)
-    # model_json = model.to_json()
-    # with open("model.json", "w") as json_file:
-    #     json_file.write(model_json)
-    # # serialize weights to HDF5
-    # model.save("model.h5")
-    # model = load_model('model.h5')
+    #create_model(fruits, labels, num_classes)
     model = load_model("model.h5")
     score = model.evaluate(fruitsTest, labelsTest, verbose=1)
-    images = os.listdir("C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//predict")
+    images = os.listdir("..//MY_data//predict")
     for img in images:
-        img_path = os.path.join("C://Users//Bogdan//FruitClassification//Fruit-Classification-CNN//MY_data//predict", img)
-        print(img)
+        img_path = os.path.join("..//MY_data//predict", img)
         predict_fruit(img_path, model)
 
